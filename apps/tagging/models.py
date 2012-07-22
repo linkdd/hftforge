f5cffa8614bebc1bf8a969f317a852a96e9c7756
David Delassus <david.jose.delassus@gmail.com>
2012-07-22 14:55:38 +0200

    Add Tag object

--- apps/tagging/models.py
+++ apps/tagging/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=25, unique=True)
+
+    def __unicode__(self):
+        return self.nam
